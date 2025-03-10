06f6d3645709986af7b9b9357efa850ee33deb1b
unknown <YY130@ccs.local>
2025-03-10 03:02:24 -0700

    question3.js

--- question3.js
+++ question3.js
@@ -7,11 +7,6 @@ const app = express();
 // Define the port number for the server
 const PORT = 3000;
 
-// Define the root route ('/') that responds with a simple text message
-app.get('/', (req, res) => {
-    res.send('Express is working! Real Jhon S. Palacio');
-});
-
 // Define the '/test' route that responds with a JSON message
 app.get('/test', (req, res) => {
     res.json({ message: 'Express is working! Real Jhon S. Palacio' });
